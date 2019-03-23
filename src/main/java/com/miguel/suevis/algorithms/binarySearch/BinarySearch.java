11e44bf18ac0fc2f59dba9ed393b644e986323d6
miguel angel suevis <miguelsuevis@miguels-MacBook-Pro.local>
2019-03-23 12:22:30 -0500

    create the structure of algorithms project

---
+++ src/main/java/com/miguel/suevis/algorithms/binarySearch/BinarySearch.java
@@ -0,0 +1,12 @@
+package com.miguel.suevis.algorithms.binarySearch;
+
+/**
+ * Binary Search algorith to search one element
+ * over the list in sorted order
+ *
+ * @author <a href="miguelangelsuevis@gmail.com">miguel angel suevis</a>
+ */
+public class BinarySearch {
+
+
+}
